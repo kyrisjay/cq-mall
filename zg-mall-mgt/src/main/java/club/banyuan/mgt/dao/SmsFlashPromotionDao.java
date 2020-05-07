832ee609b040b20993134243c8c7983da14bab4d
jay958718389@qq.com <Tj199602>
2020-05-07 12:23:53 +0800

    营销接口完成

---
+++ zg-mall-mgt/src/main/java/club/banyuan/mgt/dao/SmsFlashPromotionDao.java
@@ -0,0 +1,30 @@
+package club.banyuan.mgt.dao;
+
+import club.banyuan.mgt.dao.entity.SmsFlashPromotion;
+import club.banyuan.mgt.dao.entity.SmsFlashPromotionExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SmsFlashPromotionDao {
+    long countByExample(SmsFlashPromotionExample example);
+
+    int deleteByExample(SmsFlashPromotionExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SmsFlashPromotion record);
+
+    int insertSelective(SmsFlashPromotion record);
+
+    List<SmsFlashPromotion> selectByExample(SmsFlashPromotionExample example);
+
+    SmsFlashPromotion selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SmsFlashPromotion record, @Param("example") SmsFlashPromotionExample example);
+
+    int updateByExample(@Param("record") SmsFlashPromotion record, @Param("example") SmsFlashPromotionExample example);
+
+    int updateByPrimaryKeySelective(SmsFlashPromotion record);
+
+    int updateByPrimaryKey(SmsFlashPromotion record);
+}
